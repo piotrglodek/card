7ed7b0a64ebe892d84e750b34ef1a5409cbf0dbc
Piotr G <pioglodek@gmail.com>
2021-09-28 13:51:30 +0200

    Add temporary card states. Change decorator styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { Card } from 'components/Card/Card';
+import houseMobile from 'assets/house_mobile.png';
 
 function App() {
   return (
     <div
       style={{
         display: 'flex',
-        justifyContent: 'center',
-        backgroundColor: '#292929',
+        alignItems: 'center',
+        flexDirection: 'column',
+        backgroundColor: '#c7c7c7',
       }}>
-      <Card />
+      <Card disabled imgSrc={houseMobile} imgAlt='house' />
+      <Card disabled error imgSrc={houseMobile} imgAlt='house' />
+      <Card imgSrc={houseMobile} imgAlt='house' />
+      <Card error imgSrc={houseMobile} imgAlt='house' />
     </div>
   );
 }
